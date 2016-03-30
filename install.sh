1ea155dbe4c31c63e6a21cb72a96f6a004881ede
Jorge Aparicio <japaricious@gmail.com>
2016-03-29 21:01:25 -0500

    install.sh: refactor in small functions

--- install.sh
+++ install.sh
@@ -2,98 +2,156 @@
 
 # Installs dot files
 
-set -e
+set -ex
 set -x
 
-if [ -d ~/.dotfiles ]; then
-  git -C ~/.dotfiles pull
-else
-  git clone https://github.com/japaric/.dotfiles ~/.dotfiles
-fi
+need_cmd() {
+  if ! command -v $1 >/dev/null 2>&1; then
+    echo need $1
+    exit 1
+  fi
+}
 
-# rbenv
-if [ -d ~/.rbenv ]; then
-  git -C ~/.rbenv pull
-else
-  git clone https://github.com/rbenv/rbenv ~/.rbenv
-fi
+check_deps() {
+  need_cmd cmake
+  need_cmd curl
+  # this may not be available on exherbo in this unprefixed form
+  #need_cmd g++
+  need_cmd git
+  need_cmd python
+  need_cmd vim
+  need_cmd zsh
+}
 
-if [ -d ~/.rbenv/plugins/ruby-build ]; then
-  git -C ~/.rbenv/plugins/ruby-build pull
-else
-  mkdir -p ~/.rbenv/plugins
-  git clone https://github.com/rbenv/ruby-build ~/.rbenv/plugins/ruby-build
-fi
+fetch_dotfiles() {
+  if [ -d ~/.dotfiles ]; then
+    git -C ~/.dotfiles pull
+  else
+    git clone https://github.com/japaric/.dotfiles ~/.dotfiles
+  fi
+}
 
-# pure prompt
-if [ -d ~/.dotfiles/pure ]; then
-  git -C ~/.dotfiles/pure pull
-else
-  git clone https://github.com/sindresorhus/pure ~/.dotfiles/pure
-fi
-mkdir -p ~/.zfunctions
-ln -fs ~/.dotfiles/pure/async.zsh ~/.zfunctions/async
-ln -fs ~/.dotfiles/pure/pure.zsh ~/.zfunctions/prompt_pure_setup
+install_rbenv() {
+  if [ -d ~/.rbenv ]; then
+    git -C ~/.rbenv pull
+  else
+    git clone https://github.com/rbenv/rbenv ~/.rbenv
+  fi
 
-# base16
-if [ -d ~/.dotfiles/base16-shell ]; then
-  git -C ~/.dotfiles/base16-shell pull
-else
-  git clone https://github.com/chriskempson/base16-shell ~/.dotfiles/base16-shell
-fi
-curl https://raw.githubusercontent.com/chriskempson/base16-builder/master/templates/mutt/dark.256.muttrc.erb > ~/.dotfiles/dark.256.muttrc
+  if [ -d ~/.rbenv/plugins/ruby-build ]; then
+    git -C ~/.rbenv/plugins/ruby-build pull
+  else
+    mkdir -p ~/.rbenv/plugins
+    git clone https://github.com/rbenv/ruby-build ~/.rbenv/plugins/ruby-build
+  fi
+}
 
-# oh-my-zsh
-if [ -d ~/.dotfiles/oh-my-zsh ]; then
-  git -C ~/.dotfiles/oh-my-zsh pull
-else
-  git clone https://github.com/robbyrussell/oh-my-zsh ~/.dotfiles/oh-my-zsh
-fi
+install_pure() {
+  if [ -d ~/.dotfiles/pure ]; then
+    git -C ~/.dotfiles/pure pull
+  else
+    git clone https://github.com/sindresorhus/pure ~/.dotfiles/pure
+  fi
+  mkdir -p ~/.zfunctions
+  ln -fs ~/.dotfiles/pure/async.zsh ~/.zfunctions/async
+  ln -fs ~/.dotfiles/pure/pure.zsh ~/.zfunctions/prompt_pure_setup
+}
 
-# zsh-syntax-highlighting
-if [ -d ~/.dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-  git -C ~/.dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting pull
-else
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-fi
+install_base16() {
+  if [ -d ~/.dotfiles/base16-shell ]; then
+    git -C ~/.dotfiles/base16-shell pull
+  else
+    git clone https://github.com/chriskempson/base16-shell ~/.dotfiles/base16-shell
+  fi
+  curl https://raw.githubusercontent.com/chriskempson/base16-builder/master/templates/mutt/dark.256.muttrc.erb > ~/.dotfiles/dark.256.muttrc
+}
 
-# Vundle
-if [ -d ~/.vim/bundle/Vundle.vim ]; then
-  git -C ~/.vim/bundle/Vundle.vim pull
-else
-  mkdir -p ~/.vim/bundle
-  git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-fi
+install_oh_my_zsh() {
+  if [ -d ~/.dotfiles/oh-my-zsh ]; then
+    git -C ~/.dotfiles/oh-my-zsh pull
+  else
+    git clone https://github.com/robbyrussell/oh-my-zsh ~/.dotfiles/oh-my-zsh
+  fi
+}
 
-# create shims (only for exherbo, where only prefixed build tools exist)
-mkdir -p ~/.dotfiles/shims
-pushd ~/.dotfiles/shims
-../scripts/make-shims x86_64-pc-linux-gnu
-popd
+install_zsh_syntax_highlighting() {
+  if [ -d ~/.dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    git -C ~/.dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting pull
+  else
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.dotfiles/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  fi
+}
 
-# create symlinks
-files=(
-  .Xmodmap
-  .config/bspwm/bspwmrc
-  .config/mpv/mpv.conf
-  .config/redshift.conf
-  .config/sxhkd/sxhkdrc
-  .gdbinit
-  .gitconfig
-  .gtkrc-2.0
-  .mutt/muttrc
-  .newsbeuter/config
-  .tigrc
-  .tmux.conf
-  .urlview
-  .vimperatorrc
-  .vimrc
-  .xinitrc
-  .zprofile
-  .zshrc
-)
+install_vundle() {
+  if [ -d ~/.vim/bundle/Vundle.vim ]; then
+    git -C ~/.vim/bundle/Vundle.vim pull
+  else
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+  fi
 
-for file in ${files[*]}; do
-  mkdir -p ~/$(dirname $file)
-  ln -fns ~/.dotfiles/$file ~/$file
-done
+  vim +PluginInstall +qall
+
+  pushd ~/.vim/bundle/YouCompleteMe
+  ./install.py --racer-completer
+  popd
+}
+
+mk_shims() {
+  local prefix=x86_64-pc-linux-gnu
+
+  rm -rf ~/.dotfiles/shims
+  mkdir ~/.dotfiles/shims
+  pushd ~/.dotfiles/shims
+  for f in $(ls /usr/bin/$prefix-*); do
+    local g=$(basename $f)
+    local h=${g#$prefix-}
+    if [ -z $(echo $h | grep '\-\|\.') ]; then
+      ln -s /usr/bin/$prefix $h
+    fi
+  done
+  popd
+}
+
+mk_symlinks() {
+  local files=(
+    .Xmodmap
+    .config/bspwm/bspwmrc
+    .config/mpv/mpv.conf
+    .config/redshift.conf
+    .config/sxhkd/sxhkdrc
+    .gdbinit
+    .gitconfig
+    .gtkrc-2.0
+    .mutt/muttrc
+    .newsbeuter/config
+    .tigrc
+    .tmux.conf
+    .urlview
+    .vimperatorrc
+    .vimrc
+    .xinitrc
+    .zprofile
+    .zshrc
+  )
+
+  for file in ${files[*]}; do
+    mkdir -p ~/$(dirname $file)
+    ln -fns ~/.dotfiles/$file ~/$file
+  done
+}
+
+main() {
+  check_deps
+  fetch_dotfiles
+  install_rbenv
+  install_pure
+  install_base16
+  install_oh_my_zsh
+  install_zsh_syntax_highlighting
+  install_vundle
+  mk_shims
+  mk_symlinks
+}
+
+main
